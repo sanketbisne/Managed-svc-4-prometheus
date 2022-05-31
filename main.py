ecbffd4439a67284cb9abd88b5d715677383c956
sanket <sanketbisne@gmail.com>
2022-05-31 16:07:26 +0530

    updated code

--- main.py
+++ main.py
@@ -1,17 +1,4 @@
-# Copyright 2021 Google LLC
 
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-
-#     https://www.apache.org/licenses/LICENSE-2.0
-
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-# [START monitoring_sli_metrics_prometheus_setup]
 import random
 import time
 
@@ -42,7 +29,6 @@ PYTHON_LATENCIES_HISTOGRAM = Histogram(
 # [END monitoring_sli_metrics_prometheus_latency]
 def homePage():
     # count request
-    # [START monitoring_sli_metrics_prometheus_counts]
     PYTHON_REQUESTS_COUNTER.inc()
     # fail 10% of the time
     if random.randint(0, 100) > 90:
@@ -55,15 +41,12 @@ def homePage():
         time.sleep(random_delay)
         return "home page"
 
-
 # [START monitoring_sli_metrics_prometheus_metrics_endpoint]
 @app.route("/metrics", methods=["GET"])
 def stats():
     return generate_latest(REGISTRY), 200
 
-
 # [END monitoring_sli_metrics_prometheus_metrics_endpoint]
-
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=8080)
